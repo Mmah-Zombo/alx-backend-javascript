29e790077a1ea79fe0531881a68edd390eefd050
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-27 15:33:18 +0000

    added the new constructor

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,9 +2,9 @@ import ClassRoom from "./0-classroom";
 
 export default function initializeRooms () {
   let arr = [];
-  let klass1 = ClassRoom(19);
-  let klass2 = ClassRoom(20);
-  let klass3 = ClassRoom(34);
+  let klass1 = new ClassRoom(19);
+  let klass2 = new ClassRoom(20);
+  let klass3 = new ClassRoom(34);
 
   arr = [klass1, klass2, klass3];
   return arr;
