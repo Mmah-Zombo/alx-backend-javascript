8486f0f7f3f4aa1194eda76cf57c1e75b69dfdeb
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-27 19:16:12 +0000

    more fixing

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,8 +2,10 @@ export default class Building {
   constructor (sqft) {
     this._sqft = sqft
 
-    if (typeof (this.evacuationWarningMessage) !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage')
+    if (this.constructor !== Building) {
+      if (typeof (this.evacuationWarningMessage) !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage')
+      }
     }
   }
 
