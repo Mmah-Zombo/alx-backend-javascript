bcc2764c800dcc6becb392c4d6190efa1ae071e0
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-28 18:41:59 +0000

    added async to the function decalaration in file 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => {
       res.map((each) => ({
         status: each.status,
