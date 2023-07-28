2fceadd4b349a1c3f790557c04d30e2a18076d9c
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-28 11:45:39 +0000

    more classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,20 +1,22 @@
-import Building from './5-building'
+/* eslint-disable linebreak-style */
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor (sqft, floors) {
-    super(sqft)
-    this._floors = floors
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
   }
 
-  get floors () {
-    return this._floors
+  get floors() {
+    return this._floors;
   }
 
-  set floors (value) {
-    this._floors = value
+  set floors(value) {
+    this._floors = value;
   }
 
-  evacuationWarningMessage () {
-    return `Evacuate slowly the ${this.floors} floors`
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,26 +1,28 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable linebreak-style */
 export default class Airport {
-  constructor (name, code) {
-    this._name = name
-    this._code = code
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
   }
 
-  get name () {
-    return this._name
+  get name() {
+    return this._name;
   }
 
-  get code () {
-    return this._code
+  set name(value) {
+    this._name = value;
   }
 
-  set name (value) {
-    this._name = value
+  get code() {
+    return this._code;
   }
 
-  set code (value) {
-    this._code = value
+  set code(value) {
+    this._code = value;
   }
 
-  get [Symbol.toStringTag] () {
-    return this.code
+  get [Symbol.toStringTag]() {
+    return this.code;
   }
 }
