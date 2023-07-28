39a14423e366330920abab51744b4503d54c5544
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-28 19:10:39 +0000

    6 again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,11 +4,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => {
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
       res.map((each) => ({
         status: each.status,
         value: each.status === 'fulfilled' ? each.value : String(each.reason),
-      }));
-    });
+      }))
+    ));
 }
