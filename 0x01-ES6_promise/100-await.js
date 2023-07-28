9b4c251799e43a7dd6355a2d8dc38062642fec80
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-28 19:04:15 +0000

    100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,15 +2,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let ans;
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-
-    ans = { photo, user };
-  } catch {
-    ans = { photo: null, user: null };
+    return { photo, user };
+  } catch (err) {
+    return { photo: null, user: null };
   }
-
-  return ans;
 }
