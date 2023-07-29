5e62c24e941e8854784b5b9570a18357301e8732
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-29 02:57:39 +0000

    6 and 9

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+export default function appendToEachArrayValue(array, appendString) {
+    for (var idx in array) {
+      var value = array[idx];
+      array[idx] = appendString + value;
+    }
+  
+    return array;
+}
