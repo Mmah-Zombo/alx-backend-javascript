9b0b240cb18b269105a7e56485441c09446c6bd8
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-01 17:41:39 +0000

    task7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,4 @@
 /* eslint-disable linebreak-style */
 export default function hasValuesFromArray(set, array) {
-  array.every((e) => (set.has(e)));
-  return true;
+  return array.every((e) => (set.has(e)));
 }
