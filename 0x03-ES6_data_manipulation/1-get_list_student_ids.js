6df2252b1725b53e1917f9a7527f1169cb3f7632
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-01 12:55:37 +0000

    task1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,7 @@
 /* eslint-disable linebreak-style */
 export default function getListStudentIds(objList) {
+  if (!Array.isArray(objList)) {
+    return [];
+  }
   return objList.map((obj) => (obj.id));
 }
