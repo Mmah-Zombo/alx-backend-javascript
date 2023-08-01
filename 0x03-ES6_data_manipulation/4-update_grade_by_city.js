752cca24851729aa16c98abbe524cc117753372f
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-01 16:14:50 +0000

    task4 again

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,12 @@
 /* eslint-disable linebreak-style */
 export default function updateStudentGradeByCity(listt, city, newGrades) {
+  const defaultgrade = { grade: 'N/A'};
   const citystud = listt.filter((stud) => stud.location === city);
   const newList = citystud.map((stud) => ({
     id: stud.id,
     firstName: stud.firstName,
     location: stud.location,
-    grade: (newGrades.filter((grd) => (grd.studentId === stud.id)).pop() || 'N/A').grade,
+    grade: (newGrades.filter((grd) => (grd.studentId === stud.id)).pop() || defaultgrade).grade,
   }));
   return newList;
 }
