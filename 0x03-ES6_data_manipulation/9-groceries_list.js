b66e413bb9290a2cfcbd445aa73f984b907e6205
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-01 18:11:33 +0000

    task9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,4 +1,11 @@
 /* eslint-disable linebreak-style */
 export default function groceriesList() {
+  const map = new Map()
+    .set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5);
 
+  return map;
 }
