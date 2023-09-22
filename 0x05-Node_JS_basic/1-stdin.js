496499d5b483643adbb1d5dd351798b0ef8df327
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-22 18:29:40 +0000

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,14 @@
-/* eslint-disable linebreak-style */
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', () => {
-  const data = process.stdin.read();
-  process.stdout.write(`Your name is: ${data}`);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+
+console.log("Welcome to Holberton School, what is your name?");
+
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  console.log("This important software is now closing");
+  rl.close();
 });
