3140f5a1ad907cdf2419dda6032209904f07e35a
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-22 17:51:26 +0000

    task1 again

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,9 @@
 /* eslint-disable linebreak-style */
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  console.log(`Your name is ${data}`);
-  process.stdin.destroy();
-})
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  process.stdout.write(`Your name is ${data}`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
